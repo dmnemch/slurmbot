52ac9e7ef2fde8970c00f56e96902932c8e964b2
Dmitry Solovey <dsolovey@ist.ac.at>
2024-09-30 00:35:16 +0200

    bugs fixed

--- slurmbot/__main__.py
+++ slurmbot/__main__.py
@@ -2,8 +2,7 @@ import subprocess
 import time
 import telebot
 import yaml
-
-config = yaml.load("~/.config/slurmbot/config.yaml")
+import os
 
 def send_message(messages, token, chat):
     if not messages:
@@ -16,41 +15,50 @@ def get_finished_jobs(jobs):
     messages = []
     flag = False
     out = subprocess.run('sacct -n --format="jobid,jobname,state,exitcode"', shell=True, capture_output=True).stdout.decode('utf-8').strip().split("\n")
-    for line in out:
-        job, name, state, err = line.split()
-        if "." in job: 
-            continue
-        if state in ['COMPLETED', 'FAILED']:
-            if job not in jobs:
-                message = f"<code>{job}</code> {name + " " if not name == 'None' else ''}{state} with exit code: {err}"
-                messages.append(message)
-                jobs[job] = state
-        elif state in ['RUNNING', 'PENDING']:
-            flag = True
+    if out != ['']:
+        for line in out:
+            job, name, state, err = line.split()
+            if "." in job: 
+                continue
+            if state in ['COMPLETED', 'FAILED']:
+                if job not in jobs:
+                    message = f"<code>{job}</code> {name + " " if not name == 'None' else ''}{state} with exit code: {err}"
+                    messages.append(message)
+                    jobs[job] = state
+            elif state in ['RUNNING', 'PENDING']:
+                flag = True
+    else:
+        messages = False
     return messages, flag
 
+
 def run_bot(jobs, token, chat, mode, sleep_time):
-    telebot.TeleBot(token).send_message(chat_id=chat, text=f"Bot is running in {mode} mode with {sleep_time}s latency")
     _ = get_finished_jobs(jobs)
+    telebot.TeleBot(token).send_message(chat_id=chat, text=f"Bot is running in {mode} mode with {sleep_time}s latency")
     messages = []
     old_flag = False
     if mode == 'batch':
         while True:
             new_messages, new_flag = get_finished_jobs(jobs)
-            print(new_messages)    
-            messages += new_messages
-            if not new_flag and old_flag:
-                send_message(messages, token, chat)
-                messages = []
-            old_flag = new_flag
-            time.sleep(sleep_time)
+            if new_messages:
+                messages += new_messages
+                if not new_flag and old_flag:
+                    send_message(messages, token, chat)
+                    messages = []
+                old_flag = new_flag
+                time.sleep(sleep_time)
     elif mode == 'single':
         while True:
             messages, new_flag = get_finished_jobs(jobs)
-            send_message(messages, token, chat)
-            time.sleep(sleep_time)
+            if new_messages:
+                send_message(messages, token, chat)
+                time.sleep(sleep_time)
+
 def main():
-    run_bot(jobs = {}, token=config['token'], chat=config['token'], mode=config['mode'], sleep_time=config['sleep_time'])
+    path = os.path.expanduser('~/.config/slurmbot/config.yaml')
+    with open(path, 'r') as file:
+        config = yaml.safe_load(file)
+    run_bot(jobs = {}, token=config['token'], chat=config['chat'], mode=config['mode'], sleep_time=config['sleep_time'])
 
 if __name__ == '__main__':
     main()
