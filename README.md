2d4932808fa685d346c1b41ae23a38c8e4d47019
Dmitry Solovey <dsolovey@ist.ac.at>
2024-09-29 23:59:04 +0200

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+Simple telegram bot to notify slurm job is done.
+
+1. Install via `pip install slurmbot`
+2. Make an empty config file via `mkdir -p ~/.config/slurmbot && touch ~/.config/slurmbot/config.yaml`
+3. Make your telegram bot with BotFather, paste token into config file (like in example.yaml)
+4. Get your chat id with @RawDataBot, paste it into config file (like in example.yaml)
+5. Add `python3 $(which slurmbot)` to your ~/.bashrc or ~/.zshrc file
---
+++ slurmbot/__main__.py
@@ -0,0 +1,53 @@
+import subprocess
+import time
+import telebot
+import yaml
+
+config = yaml.load("~/.config/slurmbot/config.yaml")
+
+def send_message(messages, token, chat):
+    if not messages:
+        telebot.TeleBot(token).send_message(chat_id=chat, text='Error: message is empty')
+    for i in range(-(-len(messages) // 100)):
+        message = "\n".join(messages[i*100:i+1*100])
+        telebot.TeleBot(token).send_message(chat_id=chat, text=message, parse_mode='HTML')
+
+def get_finished_jobs(jobs):
+    messages = []
+    flag = False
+    out = subprocess.run('sacct -n --format="jobid,jobname,state,exitcode"', shell=True, capture_output=True).stdout.decode('utf-8').strip().split("\n")
+    for line in out:
+        job, name, state, err = line.split()
+        if "." in job: 
+            continue
+        if state in ['COMPLETED', 'FAILED']:
+            if job not in jobs:
+                message = f"<code>{job}</code> {name + " " if not name == 'None' else ''}{state} with exit code: {err}"
+                messages.append(message)
+                jobs[job] = state
+        elif state in ['RUNNING', 'PENDING']:
+            flag = True
+    return messages, flag
+
+def run_bot(jobs, token, chat, mode, sleep_time):
+    telebot.TeleBot(token).send_message(chat_id=chat, text=f"Bot is running in {mode} mode with {sleep_time}s latency")
+    _ = get_finished_jobs(jobs)
+    messages = []
+    old_flag = False
+    if mode == 'batch':
+        while True:
+            new_messages, new_flag = get_finished_jobs(jobs)
+            print(new_messages)    
+            messages += new_messages
+            if not new_flag and old_flag:
+                send_message(messages, token, chat)
+                messages = []
+            old_flag = new_flag
+            time.sleep(sleep_time)
+    elif mode == 'single':
+        while True:
+            messages, new_flag = get_finished_jobs(jobs)
+            send_message(messages, token, chat)
+            time.sleep(sleep_time)
+
+run_bot(jobs = {}, token=config['token'], chat=config['token'], mode=config['mode'], sleep_time=config['sleep_time'])
